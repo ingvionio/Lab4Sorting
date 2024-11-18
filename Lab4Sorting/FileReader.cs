2ede076cc27c4a9d73f354b31513dbbe0cf3ac6f
Egor Hotenov <teremok1123@mail.ru>
2024-11-18 18:33:47 +0500

    Проверка номер 2

--- Lab4Sorting/FileReader.cs
+++ Lab4Sorting/FileReader.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Lab4Sorting
 {
-    // Это читатель
+    // Это читатель снова
     public class FileReader
     {
         public string[] ReadWordsFromFile(string filePath)
